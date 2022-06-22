e1c7af9ecda71987b6f3cc9bb20c0979cbae7e71
btayfur <bilaltayfur@bayburt.edu.tr>
2022-06-22 14:41:03 +0300

    verified

---
+++ interface/LabArdwatch/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace LabArdwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Location = new System.Drawing.Point(492, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(141, 267);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test kayıt";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.richTextBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(464, 267);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gelen veri";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(6, 19);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(452, 242);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.textBox2);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(12, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(621, 56);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Bağlantı";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Portları listele";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(109, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Testi başlat";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(330, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Testi bitir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 45);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Otomatik Kaydet";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(7, 74);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 23);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Verileri temizle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(515, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "57600";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(459, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Baudrate";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 354);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Bayburt Üniversitesi Yük Deplasman Cihazı";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ interface/LabArdwatch/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.IO;
+
+
+
+namespace LabArdwatch
+{
+    public partial class Form1 : Form
+    {
+
+        string portadi1 = "";
+        private Thread okumath;
+        bool okuma = false;
+        int baudrate = 57600;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            try
+            {
+                if (comboBox1.SelectedItem.ToString() == "")
+                {
+                    MessageBox.Show("Port seçimi yapılmadı.");
+                }
+                else
+                {
+                    
+                    try
+                    {
+                        baudrate = System.Convert.ToInt32(textBox2.Text.ToString());
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Geçerli bir baudrate giriniz.");
+                        textBox2.Text = "57600";
+                    }
+                    portadi1 = comboBox1.SelectedItem.ToString();
+                    okumath = new Thread(new ThreadStart(this.Okumat));
+                    okumath.IsBackground = true;
+                    okumath.Start();
+
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void Okumat()
+        {
+            string gelenveri = "";
+            try
+            {
+                button3.Enabled = true;
+                okuma = true;
+                
+
+                portadi1 = comboBox1.SelectedItem.ToString();
+
+                SerialPort port = new SerialPort(portadi1, baudrate);
+
+
+                if (!port.IsOpen)
+                    port.Open();
+
+
+                while (okuma)
+                {
+                    gelenveri = port.ReadLine();
+                    yazdir(gelenveri);
+                }
+                port.DiscardInBuffer();
+                port.DiscardInBuffer();
+                port.Close();
+
+            }
+            catch (TimeoutException)
+            {
+                MessageBox.Show("Bir hata oluştu.");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Bağlantı Sorunu!"+e);
+            }
+
+        }
+        private void yazdir(string gelenveri)
+        {
+            richTextBox1.AppendText(gelenveri);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            okuma = false;
+            button3.Enabled = false;
+            button2.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dosyaadi = "a";
+                if (textBox1.Text == "")
+                {
+                    dosyaadi = DateTime.Now.ToString("MM_dd_yyyy HH_mm_ss");
+                }
+                else
+                {
+                    dosyaadi = textBox1.Text.ToString();
+                }
+                string fileName = dosyaadi+".txt";
+                string path = @"C:\Text_Files\my_file.txt";
+
+                while (File.Exists(fileName))
+                {
+                    fileName = fileName + "_";
+                    MessageBox.Show("Aynı adda kayıtlı dosya mevcut! \n Dosya şu isimle kaydedildi:" + fileName + "_");
+                }
+
+                StreamWriter sw = new StreamWriter(fileName);
+                string metin = richTextBox1.Text.ToString();
+                sw.WriteLine(metin);
+                sw.Close();
+
+                //File.WriteAllText(path, richTextBox1.Text.ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Verilen dosya adı uygun değil farklı bir isimle deneyin.");
+                
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
